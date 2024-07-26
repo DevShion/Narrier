4879df151c452cc43f481b0c5cc64edc75a9c953
DevShion <shionpro.developer@gmail.com>
2024-07-27 01:55:19 +0900

    comment

--- DIST/module/index.js
+++ DIST/module/index.js
@@ -3,14 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Narrier = void 0;
 const net_1 = __importDefault(require("net"));
 class Narrier {
     addrRegex = /^(([a-zA-Z\-\.0-9]+):)?(\d+)$/; // parse "80" and "localhost:80" or even "42mEANINg-life.com:80"
     addr;
     server = null;
-    constructor(fromString, toString) {
-        const from = this.addrRegex.exec(fromString);
-        const to = this.addrRegex.exec(toString);
+    constructor(options) {
+        const from = this.addrRegex.exec(options.from);
+        const to = this.addrRegex.exec(options.to);
         if (from === null || to === null) {
             throw new Error(`Invalid address: ${from} ${to}`);
         }
@@ -42,5 +43,6 @@ class Narrier {
         this.server?.close();
     };
 }
+exports.Narrier = Narrier;
 ;
 exports.default = Narrier;
--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import net from "net";
 
-class Narrier {
+type NarierOptions = { from: string, to: string };
+
+export class Narrier {
     private addrRegex = /^(([a-zA-Z\-\.0-9]+):)?(\d+)$/; // parse "80" and "localhost:80" or even "42mEANINg-life.com:80"
     private addr: {
         from: { host: string, port: number, },
@@ -9,9 +11,9 @@ class Narrier {
 
     private server = null as net.Server | null;
 
-    constructor(fromString: string, toString: string) {
-        const from = this.addrRegex.exec(fromString);
-        const to = this.addrRegex.exec(toString);
+    constructor(options: NarierOptions) {
+        const from = this.addrRegex.exec(options.from);
+        const to = this.addrRegex.exec(options.to);
         if (from === null || to === null) { throw new Error(`Invalid address: ${from} ${to}`); };
         this.addr = {
             from: { host: from[2], port: Number(from[3]), },
@@ -33,8 +35,8 @@ class Narrier {
         }).listen(addr.from.port, addr.from.host, () => {
             console.log(`
                 Port forwarding
-                from: ${ this.addr.from.host + ":" + this.addr.from.port }
-                to: ${ this.addr.to.host + ":" + this.addr.to.port }`
+                from: ${this.addr.from.host + ":" + this.addr.from.port}
+                to: ${this.addr.to.host + ":" + this.addr.to.port}`
             );
         });
     };
